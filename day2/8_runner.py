18f0921bd0001d13a3cfa99ba7ddaf257e1bf4ca
vg7venu <vg7venu@gmail.com>
2020-04-20 20:17:46 +0530

    day 2: runner *revamp of runners program by brother.

--- day2/8_runner.py
+++ day2/8_runner.py
@@ -3,19 +3,32 @@ if __name__ == '__main__':
     arr = list(map(int, input().split()[:n]))
     big = 0
     sbig = 0
-    for i in range(n):
-        if(arr[i]>big and arr[i]!= big):
+    # for i in range(n):
+    #     if(arr[i]>big and arr[i]!= big):
+    #         sbig = big
+    #         big = arr[i]
+    #     else:
+    #         if(arr[i] != big):
+    #             if(sbig<arr[i]):
+    #                 sbig = arr[i]
+    #             else:
+    #                 if(sbig > arr[i]):
+    #                     sbig = arr[i]
+    #                     if (sbig > big):
+    #                         temp = big
+    #                         big = sbig
+    #                         sbig = temp    
+    # print(sbig)
+    big = sbig = arr[0];
+    for item in range(1,n):
+        if(big<arr[item] ):
             sbig = big
-            big = arr[i]
+            big = arr[item]
+            continue
+        elif(big == arr[item]):
+            continue
         else:
-            if(arr[i] != big):
-                if(sbig<arr[i]):
-                    sbig = arr[i]
-                else:
-                    if(sbig > arr[i]):
-                        sbig = arr[i]
-                        if (sbig > big):
-                            temp = big
-                            big = sbig
-                            sbig = temp    
+            if(sbig == big or sbig <= arr[item]):
+                sbig = arr[item]
+                continue
     print(sbig)
